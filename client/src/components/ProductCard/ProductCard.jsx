7e2301eee46d092a65a1195ea380ce4907bc1ee8
jregues <jregues@me.com>
2024-06-02 20:35:57 -0400

    added /allGames data onto productCards

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -4,11 +4,11 @@ import styles from  './ProductCard.module.css';
 const ProductCard = ({ product }) => {
   return (
     <div className={`${styles.productCard} col-lg-3`}>
-      <img src={product.image} alt={product.name} className={styles.productImage} />
+      {/* <img src={product.image} alt={product.name} className={styles.productImage} /> */}
       <div className={styles.productDetails}>
-        <h2>{product.name}</h2>
+        <h2>{product.title}</h2>
         <p>Moby Score:</p>
-        <p>{product.description}</p>
+        <p>{product.moby_url}</p>
         <button className={styles.productButton}>Add to favorite</button>
       </div>
     </div>
